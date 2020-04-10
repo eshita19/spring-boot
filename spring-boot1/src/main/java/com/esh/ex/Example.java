b6e3015350467cee6c0eac37fb8d2eb3f7125dc1
emathur <emathur@cisco.com>
2020-04-10 09:59:35 +0530

    @Value annotation

--- spring-boot1/src/main/java/com/esh/ex/Example.java
+++ spring-boot1/src/main/java/com/esh/ex/Example.java
@@ -1,13 +1,16 @@
 package com.esh.ex;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Example {
+	@Value(value = "${spring.devtools.remote.secret}")
+	private String remoteSecret;
 
     @RequestMapping("/")
     String home() {
-        return "Hello World! This is Eshita Mathur";
+        return "Hello World! This is Eshita Mathur. Admin enabled is " + remoteSecret;
     }
 }
