e528fa53b98ed1b455cf0fa854625bd66c69e32c
emathur <emathur@cisco.com>
2020-04-10 10:36:52 +0530

    Config properties

--- spring-boot1/src/main/java/com/esh/ex/Example.java
+++ spring-boot1/src/main/java/com/esh/ex/Example.java
@@ -1,5 +1,6 @@
 package com.esh.ex;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
 	@Value(value = "${spring.devtools.remote.secret}")
 	private String remoteSecret;
+	
+	@Autowired
+	private EshProps eshProps;
 
-    @RequestMapping("/")
+    public String getRemoteSecret() {
+		return remoteSecret;
+	}
+
+	public void setRemoteSecret(String remoteSecret) {
+		this.remoteSecret = remoteSecret;
+	}
+
+	public EshProps getEshProps() {
+		return eshProps;
+	}
+
+	public void setEshProps(EshProps eshProps) {
+		this.eshProps = eshProps;
+	}
+
+	@RequestMapping("/")
     String home() {
-        return "Hello World! This is Eshita Mathur. Admin enabled is " + remoteSecret;
+        return "Hello World! This is " +  eshProps.getName()  + ". Admin enabled is " + remoteSecret;
     }
 }
